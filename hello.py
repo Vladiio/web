67f604cc9f3f1a447aac47d48236f6adb10ed118
Vlad <vladpochta5@gmail.com>
2017-08-11 09:39:34 +0000

    Finish static section

--- hello.py
+++ hello.py
@@ -1,10 +1,12 @@
-import json
 
 
 def wsgi_application(environ, start_response):
     status = "200 OK"
+    qs = environ.get('QUERY_STRING', '')
     headers = [('Content-Type', 'text/plain')]
     start_response(status, headers)
-    body = 'Hello world'
+    body = bytes('\n'.join(qs.split('&')), 'utf-8')
+    with open('text.txt', 'wb') as file:
+        file.write(body)
     return [body]
 
