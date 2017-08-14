af6f6f5df787db845acf43ac243941c952adc435
Vlad <vladpochta5@gmail.com>
2017-08-14 14:14:17 +0000

    Add app to installed_apps

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,7 +1,7 @@
 from __future__ import unicode_literals
 
 from django.db import models
-from django.conf inport settings
+from django.conf import settings
 
 
 class QuestionManager(models.Manager):
@@ -9,17 +9,18 @@ class QuestionManager(models.Manager):
     def new(self):
         return self.get_queryset()[:5]
     
-    def populer(self):
+    def popular(self):
         return self.get_queryset().order_by('rating')
 
 
 class Question(models.Model):
-    title = models.CharField(max_lenght=120)
+    title = models.CharField(max_length=120)
     text = models.TextField()
     added_at = models.DateField()
     rating = models.IntegerField()
     author = models.ForeignKey(settings.AUTH_USER_MODEL)
-    likes = models.ManyToManyField(settings.AUTH_USER_MODEL)
+    likes = models.ManyToManyField(settings.AUTH_USER_MODEL,
+                                   related_name='liked')
     
     objects = QuestionManager()
     
