ae7a82006a62ee1d524c70ab15f31eecd82384c4
Vlad <vladpochta5@gmail.com>
2017-08-16 10:48:26 +0000

    Views

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -3,14 +3,17 @@ from __future__ import unicode_literals
 from django.db import models
 from django.conf import settings
 
+from django.core.paginator import Paginator
+
 
 class QuestionManager(models.Manager):
     
-    def new(self):
-        return self.get_queryset()[:5]
-    
-    def popular(self):
-        return self.get_queryset().order_by('rating')
+    def new(self, page_number=1):
+        return paginate(self, page_number)
+ 
+    def popular(self, page_number=1):
+        qs = self.order_by('rating')        
+        return paginate(qs, page_number)
 
 
 class Question(models.Model):
@@ -33,4 +36,11 @@ class Answer(models.Model):
     added_at = models.DateField()
     question = models.ForeignKey(Question)
     author = models.ForeignKey(settings.AUTH_USER_MODEL)
+
+
+def paginate(request, qs, limit=10):
+    page_number = request.GET.get('page', 1)
+    paginator = Paginator(qs, limit)
+    page = paginator.page(page_number)
+    return page        
 
--- ask/qa/views.py
+++ ask/qa/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Question
+
+
+def home(request):
+    page = request.GET.get('page', 1)
+    qs = Question.objects.new(page)
+    return render(request, 'qa/home.html', {'qa': qs.object_list,
+                                            'page': qs})
+
+def popular(request):
+    page = request.GET.get('page', 1)
+    qs = Question.objects.populer(page)
+    return render(request, 'qa/popular.html', {'qa': qs.object_list,
+                                               'page': qs})
